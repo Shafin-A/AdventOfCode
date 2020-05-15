98e87fff81acaba87485d5dd1289dfc429e86c60
Shafin <kazishafin@gmail.com>
2020-05-15 18:22:52 -0400

    day3 works

--- day3/day3.py
+++ day3/day3.py
@@ -5,32 +5,30 @@ def build_path(path):
     coords = []
     x = 0
     y = 0
+    
     for dir in path:
         if dir[0] == 'R':
-            right = int(dir[1 : len(dir)])
+            right = int(dir[1:])
             for i in range(right):
                 coords.append((x + i, y))
             x += right
         if dir[0] == 'L':
-            left = int(dir[1 : len(dir)])
+            left = int(dir[1:])
             for i in range(left):
                 coords.append((x - i, y))
             x -= left
         if dir[0] == 'U':
-            up = int(dir[1 : len(dir)])
+            up = int(dir[1:])
             for i in range(up):
                 coords.append((x, y + i))
             y += up
         if dir[0] == 'D':
-            down = int(dir[1 : len(dir)])
+            down = int(dir[1:])
             for i in range(down):
                 coords.append((x, y - i))
             y -= down
-        coords.append((x, y))
     return coords
         
-
-
 f = open("input.txt", "r")
 
 path_1 = f.readline().split(",")
@@ -39,16 +37,33 @@ path_2 = f.readline().split(",")
 coords_1 = build_path(path_1)
 coords_2 = build_path(path_2)
 
-min_dist = 9999999
-for i in coords_1:
-    for j in coords_2:
-        if i == (0, 0) or j == (0, 0):
-            continue
-        if i == j:
-            x, y = i
+## this is very slowwww, but it works!
+##min_dist = 9999999
+##for i in coords_1:
+##    for j in coords_2:
+##        print(i, j)
+##        if i == j:
+##            if i == (0, 0) or j == (0, 0):
+##                continue
+##            x, y = i
+##
+##            dist = manhattan(x, y, 0, 0)
+##            if dist < min_dist:
+##                min_dist = dist
 
-            dist = manhattan(x, y, 0, 0)
-            if dist < min_dist:
-                min_dist = dist
+intersect = set(coords_1).intersection(coords_2)
+intersect = intersect.difference([(0,0)])
+
+min_dist = 99999
+for x,y in intersect:
+    dist = manhattan(x, y, 0, 0)
+    if dist < min_dist:
+        min_dist = dist
+
+combined_steps = []
+for i in intersect:
+    combined_steps.append(coords_1.index(i) + coords_2.index(i))
 
 print(min_dist)
+print(min(combined_steps))
+
